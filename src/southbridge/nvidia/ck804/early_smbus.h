f95e5d9b059081c7674dcb0863be37e772f1ceac
John Doe <john.doe@example.com>
2022-03-13 09:37:42 -0400

    ck804: early_smbus.h changes from kgpe-d16

---
+++ src/southbridge/nvidia/ck804/early_smbus.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+int ck804_smbus_read_byte(unsigned int, unsigned int, unsigned int);
+int ck804_smbus_write_byte(unsigned int, unsigned int, unsigned int, unsigned char);
+void enable_smbus(void);
+int smbus_read_byte(unsigned int, unsigned int);
+int smbus_write_byte(unsigned int, unsigned int, unsigned char);
