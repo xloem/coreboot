ceaa45d4b631f46de45431401a5cc750f0731a74
Karl Semich <0xloem@gmail.com>
2022-03-14 22:05:04 -0400

    Revert "amdfam10: Clean leftover prototypes"
    This reverts commit 6a23352515defc92fca208bdcf0d345d4542d6ce.

--- src/include/cpu/amd/car.h
+++ src/include/cpu/amd/car.h
@@ -4,6 +4,10 @@
 #include <arch/cpu.h>
 
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx);
+asmlinkage void *post_cache_as_ram(void);
+asmlinkage void cache_as_ram_new_stack(void);
+
+void disable_cache_as_ram(void);
 
 void *asmlinkage romstage_main(unsigned long bist);
 
